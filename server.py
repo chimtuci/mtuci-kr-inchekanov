22d4f5c0e8c96dfda8e03f12f3dbc981df58d6cd
ChekanovIN_UBST2203 <chekanov@KALI-22286.kali>
2025-05-14 13:22:59 +0300

    Flake8 Check commit

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route('/chekanov')
+
+
 def hello_world():
-	return "Hello, World!"
+ return "Hello, World!"
+
 
 if __name__ == '__main__':
-	app.run(port=8050, debug=True)
+ app.run(port=8050, debug=True)
